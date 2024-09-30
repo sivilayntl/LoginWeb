ff77c96714ed10eb60d2ce8eb05ac3d953a00300
Seedemo <sivilay900@gmail.com>
2024-09-30 15:54:22 +0700

    import error

---
+++ components/UserInfo.jsx
@@ -0,0 +1,20 @@
+'use client';
+import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+export default function UserInfo() {
+
+    const {data:session} = useSession();
+
+    return <div className="grid place-items-center h-screen">
+        <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6">
+            <div>
+                Name: <pan className="font-bold">{session?.user?.name}</pan>
+            </div>
+            <div>
+                Email: <pan className="font-bold">{session?.user?.email}</pan>
+            </div>
+            <button onClick={() => signOut} className="bg-red-500 text-white font-bold px-6 py-2 mt-3">Log Out</button>
+        </div>
+    </div>;
+}
